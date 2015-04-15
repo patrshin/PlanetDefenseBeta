64491b63d1771eb53b96c25b7d121bcabd99911c
jcorks <jcorkery@umich.edu>
2015-04-15 00:06:48 -0400

    Added new title screen

---
+++ Assets/Scripts/etc/Pulsator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulsator : MonoBehaviour {
+
+	Material m;
+
+	float time;
+	// Use this for initialization
+	void Start () {
+		m = GetComponent<SpriteRenderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		time += .1f;
+		Color newColor = m.color;
+		newColor.a = .5f + .5f*Mathf.Sin (time);
+		m.color = newColor;
+	}
+}
---
+++ Assets/Scripts/etc/Translator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Translator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (new Vector3 (.1f, 0f, 0f));
+	}
+}
