c464b58fca96017f862ea6645e918b830f0775a0
lee-CS <john.flo.lee@gmail.com>
2015-04-15 08:05:44 -0400

    more :camel:s

---
+++ Assets/Scripts/level_manager/lvl2_Intro.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lvl2_Intro : MonoBehaviour {
+
+	public GameObject sun;
+	public GameObject explosion;
+
+	public bool expl;
+	public bool hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (expl && hit) {
+			if(transform.localScale.x > 400) {
+				Application.LoadLevel("Level_2_Intro2");
+			}
+		}
+
+	}
+
+	void OnTriggerEnter(Collider c) {		
+		if (c.gameObject.CompareTag ("sun")) {
+			explosion.GetComponent<level2_script>().expansion_rate = 2;
+			explosion.GetComponent<lvl2_Intro>().hit = true;
+			Destroy(gameObject);
+
+		}
+	}
+}
--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 1850
+                Build = 1878
 			};
 		}
 		public static VersionInfo UnityVersion()
