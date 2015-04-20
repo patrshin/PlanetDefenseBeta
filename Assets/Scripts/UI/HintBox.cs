a651d1100db272bb3910ce372d791d02be084149
Zachary Nowicki <nowicki.zm7@gmail.com>
2015-04-19 22:21:40 -0400

    Hint Box!!!

---
+++ Assets/Scripts/UI/HintBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HintBox : MonoBehaviour {
+
+	string[] option = new string[]{
+		"Stack with your patner's P.A.D.L. to fire enhaced weapons!",
+		"Hold down RB to charge your Laser!",
+		"Your secondary shot will explode after a short period of time",
+		"P.A.D.L's are indestructable, so don't worry about getting hit",
+		"Sheilded enemies are not effected by lasers"
+	};
+	// Use this for initialization
+	void Start () {
+		int index = Random.Range (0, option.Length-1);
+		gameObject.GetComponent<Text> ().text = option [index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
