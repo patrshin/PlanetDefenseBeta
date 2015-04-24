30ef3300c2f228d3abd48f7c55ef0f58a5612487
patrshin <patrshin@umich.edu>
2015-04-23 23:34:26 -0400

    fixed some things in outro level 3

--- Assets/Scripts/level_manager/level3Outro.cs
+++ Assets/Scripts/level_manager/level3Outro.cs
@@ -4,16 +4,10 @@ using System.Collections;
 public class level3Outro : MonoBehaviour {
 
 	private bool entering = false;
-	private float enterTime = 5f;
+	private float enterTime = 4f;
 	private float timer = 0f;
 	// Use this for initialization
 	void Start () {
-		Transform[] allChildren = GetComponentsInChildren<Transform>();
-		foreach (Transform child in allChildren) {
-			if (child.name == "playerBoost") {
-				child.GetComponent<ParticleSystem>().enableEmission = true;
-			}
-		}
 	}
 	
 	// Update is called once per frame
@@ -28,12 +22,21 @@ public class level3Outro : MonoBehaviour {
 				foreach (Transform child in allChildren) {
 					if (child.name == "playerBoost") {
 						child.GetComponent<ParticleSystem>().enableEmission = false;
+						child.GetComponent<AudioSource>().Stop();
 					}
 				}
+				if (!GetComponent<AudioSource>().isPlaying)
+					GetComponent<AudioSource>().Play();
 				Vector3 expanded = transform.localScale;
 				if (expanded.x > 0) {
 					expanded.x -= 0.035f;
 					expanded.y -= 0.035f;
+				}
+				else {
+					Destroy(gameObject);
+					Application.LoadLevel("level_4_Intro");
+					return;
+
 				}
 				transform.localScale = expanded;
 				this.transform.Rotate(Vector3.forward * Time.deltaTime * 40f);
